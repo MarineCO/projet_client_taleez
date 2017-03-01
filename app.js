b972ed9ab18762b909a7ee1a745e6725613195fe
rachdi <bigking3100@gmail.com>
2017-03-01 14:52:56 +0100

    runtime send message et liaison des fichier de l'extension recuperération des données nom prenom job premiere affichage sur le popup de l'extenstion

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-var headings = document.evaluate("/html/body//h1", document, null, XPathResult.ANY_TYPE, null); 
+
+
+chrome.runtime.onMessage.addListener(
+	function(request, sender, sendResponse) {
+
+		var headings = document.evaluate("//*[@id="ember3887"]/div[3]/div[1]/h1", document, null, XPathResult.ANY_TYPE, null); 
 /* Search the document for all h2 elements.  
 * The result will likely be an unordered node iterator. */
 var thisHeading = headings.iterateNext(); 
@@ -10,7 +15,7 @@ while (thisHeading) {
 }
 console.log("votre nom est :"+name);
 
-var headings = document.evaluate("/html/body//h2", document, null, XPathResult.ANY_TYPE, null); 
+var headings = document.evaluate("//*[@id="ember3843"]/div[3]/div[1]/h2", document, null, XPathResult.ANY_TYPE, null); 
 /* Search the document for all h2 elements.  
 * The result will likely be an unordered node iterator. */
 var thisHeading = headings.iterateNext(); 
@@ -21,3 +26,11 @@ while (thisHeading) {
 	// document.getElementById('job').innerHTML=alertText;
 }
 console.log("votre travaille :"+job);
+
+
+
+
+var reponse={name,job};
+
+sendResponse(reponse);
+});
