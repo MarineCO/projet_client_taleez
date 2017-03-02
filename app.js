540de2b314e4387ee0971ce52606245c9551d66f
marineco <marinecolonge@gmail.com>
2017-03-02 18:35:01 +0100

    récupération des infos de l'app.js vers le popup.js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
 
 
-		var response = ["/html/body//h1[contains(@class, 'name')]", "/html/body//h2[contains(@class, 'headline')]" , "/html/body//div[contains(@class, 'pv-contact-info__ci-container')]"];
-
+		var response = ["/html/body//h1[contains(@class, 'name')]", "/html/body//h2[contains(@class, 'headline')]"];
+		var tab = [];
 
 		response.forEach(function(element) {
 			var headings = document.evaluate(element, document, null, XPathResult.ANY_TYPE, null);
@@ -14,10 +14,10 @@ chrome.runtime.onMessage.addListener(
 				element += thisHeading.textContent + "\n";
 				thisHeading = headings.iterateNext();
 			}
-			console.log(element);
 
+			tab.push(element);
 		});
-		sendResponse(response['element']);
 
+			sendResponse(tab);
 	}
-	);
+);
